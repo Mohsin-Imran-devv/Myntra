b435295dfefaf28883a90b937307747020ac102d
Mohsin-Imran-devv <2.mohsin.ali.2@gmail.com>
2026-05-08 21:59:47 +0500

    Add new root

--- 2-actual-backend/app.js
+++ 2-actual-backend/app.js
@@ -16,6 +16,13 @@ app.use(bodyParser.json());
 app.use(cors());
 
 // Routes
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Backend is running",
+  });
+});
+
 app.get("/api/products", async (req, res) => {
   try {
     const products = await Product.find();
